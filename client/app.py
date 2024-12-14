f90f24d6fa1c921032a058e773c9176fcfa03672
awa Karamoko <awa.karamoko@univ-lyon2.fr>
2024-12-14 15:19:27 +0100

    update

--- client/app.py
+++ client/app.py
@@ -1,11 +1,17 @@
+import os
 import streamlit as st
 import requests
 import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 # Charger les métriques sauvegardées (assure-toi que 'metrics.pkl' est dans le bon répertoire)
-metrics = joblib.load("metrics.pkl") 
+#metrics = joblib.load("metrics.pkl") 
+
+# Utilisation d'un chemin relatif
+file_path = os.path.join("server", "metrics.pkl")
+metrics = joblib.load(file_path)
 
 # URL de l'API FastAPI
 API_URL = "http://server:8000/predict/"
